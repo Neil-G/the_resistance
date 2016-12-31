ba26d48e5623e5091dd87b8d2908f54b35ea80cc
Neil-G <neil@jigsaw.xyz>
2016-12-30 20:14:27 -0500

    add selectSpies, setInitialLeader, calculateMissionSize

--- src/Game/index.js
+++ src/Game/index.js
@@ -3,15 +3,22 @@
 const immutable = require('immutable');
 const fromJS = immutable.fromJS;
 const Map = immutable.Map;
+const List = immutable.List;
 
 
 // REDUCER
 module.exports.reducer = (state = fromJS(initialState), action) => {
   switch(action.type){
     case 'addPlayerToGame':
-      return addPlayerToGame(state, action.player)
-      case 'removePlayerFromGame':
-        return removePlayerFromGame(state, action.playerId)
+      return addPlayerToGame(state, action.player);
+    case 'removePlayerFromGame':
+      return removePlayerFromGame(state, action.playerId);
+    case 'setSpies':
+      return setSpies(state);
+    case 'setInitialLeader':
+      return setInitialLeader(state);
+    case 'selectPlayerForMission':
+      return selectPlayerForMission(state, action.playerId);
     default:
       return state
   }
@@ -21,7 +28,7 @@ module.exports.reducer = (state = fromJS(initialState), action) => {
 // INITIAL STATE
 const initialState = {
   id: undefined,
-  spyIdArray: [],
+  spyIds: [],
   players: [],
   pastMissions: [],
   currentMission: {
@@ -49,16 +56,34 @@ function removePlayerFromGame(state, playerId) {
   return state.setIn(['players'], updatedPlayersList);
 }
 
-function chooseSpies(state){
-  if (state.get('players').size < 5) return state
+function setSpies(state){
+  const numberofPlayers = state.get('players').size;
+  if (numberofPlayers < 5) return state
+  const numberOfSpies = Math.ceil( numberofPlayers / 3);
+  let spyIndexes = [];
+  while (spyIndexes.length < numberOfSpies){
+    let randomIndex = Math.floor(Math.random() * numberofPlayers)
+    if (!spyIndexes.includes(randomIndex)) spyIndexes.push(randomIndex)
+  }
+  const updatedSpyIds = spyIndexes.map((index) => state.get('players').toJS()[Number(index)].id)
+  return state.setIn(['spyIds'], List(updatedSpyIds));
 }
 
-function chooseInitialLeader(state) {
-  if (state.get('players').size < 5) return state
+function setInitialLeader(state) {
+  const numberofPlayers = state.get('players').size;
+  if (numberofPlayers < 5) return state
+  let randomIndex = Math.floor(Math.random() * numberofPlayers)
+  return state.setIn(['currentMission', 'leaderId'], state.get('players').toJS()[Number(randomIndex)].id)
 }
 
-function selectPlayerForMission(state, id) {
-  // pushes id to playerIds.SelectedForMission
+function selectPlayerForMission(state, playerId) {
+  // only leader can select
+  // id must be valid player id
+  // mission must not be full
+  return state.setIn(
+    ['currentMission', 'playerIdsSelectedForMission'],
+    state.get('currentMission').get('playerIdsSelectedForMission').push(playerId)
+  );
 }
 
 function unselectPlayerForMission(state, id) {
@@ -89,4 +114,16 @@ function castMissionVote(state, vote) {
 
 function endMission(state) {
   // move currentMission to PastMissionsArray
+}
+
+function calculateMissionSize(numberOfPlayers, missionNumber) {
+  if (NumberOfPlayers == 5) {
+    return [2, 3, 2, 3, 3][ missionNumber - 1 ]
+  } else if (NumberOfPlayers == 6) {
+    return [2, 3, 4, 3, 4][ missionNumber - 1 ]
+  } else if (NumberOfPlayers == 7) {
+    return [2, 3, 3, 4, 4][ missionNumber - 1 ]
+  } else if ((NumberOfPlayers > 7) && (NumberOfPlayers < 11)) {
+    return [3, 4, 4, 5, 5][ missionNumber - 1 ]
+  }
 }
--- tests/store.js
+++ tests/store.js
@@ -7,33 +7,66 @@ const Map = immutable.Map;
 let store = require('./../src/store')
 
 test('game reducer', (t) => {
-  t.plan(3);
-  const initialGameState = store.getState().game;
+  t.plan(6);
+
+  // INITIALIZE GAME
   t.true(
-    Map.isMap(initialGameState),
+    Map.isMap(store.getState().game),
     'initializes game as an Immutable Map'
   );
 
+
+  // ADD PLAYER
   const mockPlayer1 = { id: '1', name: 'Alan' }
-  store.dispatch({
-    type: 'addPlayerToGame',
-    player: mockPlayer1
-  })
-
-
+  store.dispatch({ type: 'addPlayerToGame', player: mockPlayer1 })
   t.equal(
     store.getState().game.get('players').toJS()[0],
     mockPlayer1,
     'can add a player to the game'
   )
 
-  store.dispatch({
-    type: 'removePlayerFromGame',
-    playerId: '1'
-  })
 
-  t.true(
-    store.getState().game.get('players').size == 0,
+  // REMOVE PLAYER
+  store.dispatch({ type: 'removePlayerFromGame',playerId: '1' })
+  t.equal(
+    store.getState().game.get('players').size, 0,
     'can remove a player from the game'
   )
-})
+
+
+  // SET SPIES
+  const mockPlayer2 = { id: '2', name: 'Beth' };
+  const mockPlayer3 = { id: '3', name: 'Carl' };
+  const mockPlayer4 = { id: '4', name: 'Dan' };
+  const mockPlayer5 = { id: '5', name: 'Erin' };
+  store.dispatch({ type: 'addPlayerToGame', player: mockPlayer1 });
+  store.dispatch({ type: 'addPlayerToGame', player: mockPlayer2 });
+  store.dispatch({ type: 'addPlayerToGame', player: mockPlayer3 });
+  store.dispatch({ type: 'addPlayerToGame', player: mockPlayer4 });
+  store.dispatch({ type: 'addPlayerToGame', player: mockPlayer5 });
+  store.dispatch({ type: 'setSpies' });
+  t.equal(
+    store.getState().game.get('spyIds').size, 2,
+    'can set spies'
+  );
+
+  // SET LEADER
+  store.dispatch({ type: 'setInitialLeader' })
+  t.notEqual(
+    store.getState().game.get('currentMission').get('leaderId'), undefined,
+    'can set initial leader'
+  )
+
+  // Select a Player for the current mission
+  store.dispatch({ type: 'selectPlayerForMission', playerId: '1' })
+  t.equal(
+    store.getState().game.get('currentMission').get('playerIdsSelectedForMission').size, 1,
+    'can select a player for the mission'
+  )
+  showGameState(store)
+});
+
+
+function showGameState(store){
+  console.log(store.getState().game)
+}
